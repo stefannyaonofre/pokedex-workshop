ec7cb6cea53cf5a8d82c223ae12e8dc5df745f85
Stefannya <stefannyaonofre@gmail.com>
2023-06-23 12:08:22 -0500

    cration getPokemons

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -1,1 +1,10 @@
 import "../styles/style.scss"
+import { getPokemons } from "./services/getPokemones.js"
+
+// const selection = document.querySelector('bulbasaur');
+
+// getPokemons();
+
+// selection.addEventListener('click', () => {
+//     console.log('hice click')
+// })
---
+++ src/app/scripts/services/data.js
@@ -0,0 +1,3 @@
+const URL_API = 'https://pokeapi.co/api/v2/pokemon/'
+
+export default URL_API;
