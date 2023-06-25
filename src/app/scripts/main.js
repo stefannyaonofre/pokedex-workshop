ac56fdd2ef291396250b9de39ef2465c7544ec7e
Stefannya <stefannyaonofre@gmail.com>
2023-06-25 17:31:59 -0500

    creation filtro

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -1,11 +1,18 @@
 import "../styles/style.scss"
 import printPokemons from "./UI/printPokemons";
-import validarPokemon from "./UI/validarPokemons";
+import validarPokemon, { validarFiltro } from "./UI/validarPokemons";
 import { getPokemons } from "./services/getPokemones.js"
 
 const main = document.getElementById('main');
+const formFiltro = document.getElementById('form');
 
 // const pokemonesPrincipales = ['bulbasaur', 'metapod', 'charmander', 'charizard'];
+
+formFiltro.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validarFiltro(formFiltro, main);
+
+})
 
 document.addEventListener('DOMContentLoaded', () => {
 
